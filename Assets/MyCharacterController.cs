2417713d12354f65a1f337c30f735d254af2011a
Marco Secchi <loziosecchi@gmail.com>
2017-06-05 18:51:38 +0200

    Add materials and scene.

---
+++ Assets/MyCharacterController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyCharacterController : MonoBehaviour {
+
+	// Use this for initialization
+//	void Start () {
+//		
+//	}
+	
+	// Update is called once per frame
+//	void Update () {    
+//		
+//	}
+
+	void OnTriggerEnter(Collider other) {
+		GameObject go = other.gameObject;
+		Transform t = other.transform;
+
+		Debug.Log ("Sei entrato in " + go.name);
+		Debug.Log ("Che si trova in  " + t.position.x + ", " + t.position.y  + ", " + t.position.z);
+	}
+}
---
+++ Assets/MyGameController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyGameController : MonoBehaviour {
+
+	public bool debugMode;
+
+	public Material environmentMaterial;
+	public Material targetMaterial;
+
+	// Use this for initialization
+	void Start () {
+		if (debugMode)
+			return;
+		
+		GameObject go = GameObject.Find ("Target");
+		go.GetComponent<Renderer> ().material = targetMaterial;
+		GameObject[] goList = GameObject.FindGameObjectsWithTag ("EnvironmentElement");
+		foreach (GameObject element in goList) {
+			Debug.Log (element);
+			element.GetComponent<Renderer> ().material = environmentMaterial;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
