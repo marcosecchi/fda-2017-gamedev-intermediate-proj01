bfad98f6f1783bd330181e2db1b824e6b8c7591f
Marco Secchi <loziosecchi@gmail.com>
2017-06-06 07:40:55 +0200

    Improve comments.

--- Assets/MyCharacterController.cs
+++ Assets/MyCharacterController.cs
@@ -17,10 +17,29 @@ public class MyCharacterController : MonoBehaviour {
 	// Quando il gameobject cui appartiene questo componente entra in un trigger,
 	// stampa alla console il nome dell'oggetto cui appartiene il trigger e la sua posizione
 	void OnTriggerEnter(Collider other) {
+		
+		// Recupero il gameobject che contiene il Collider intercettato
 		GameObject go = other.gameObject;
+
+		// Recupero il transform (posizione, rotazione e scala) del gameobject
+		// che contiene il Collider intercettato
 		Transform t = other.transform;
 
+		// NOTA: scrivere
+		//        other.gameobject.transform
+		// oppure
+		//        other.transform
+		// è la medesima cosa (sono entrambe scorciatoie per lo stesso transform)
+		//
+		// allo stesso modo, scrivere
+		//        other.transform.gameobject
+		// ritorna lo stesso risultato di
+		//        other.gameobject
+
+		// Traccio a console il nome del gameobject
 		Debug.Log ("Sei entrato in " + go.name);
+
+		// Traccio a console la posizione del gameobject
 		Debug.Log ("Che si trova in  " + t.position.x + ", " + t.position.y  + ", " + t.position.z);
 	}
 }
--- Assets/MyGameController.cs
+++ Assets/MyGameController.cs
@@ -17,18 +17,37 @@ public class MyGameController : MonoBehaviour {
 
 	// Callback di Unity per inizializzare la scena
 	void Start () {
-		// Se ci si trova in devMode, si ritorna senza effettuare nulla
+		// Se ci si trova in devMode, si esce dalla funzione
+		// senza effettuare nessuna operazione
 		if (devMode)
 			return;
-		
+
+		// Cerco in scena un oggetto chiamato 'Target'
+		// NOTA: per semplificare do per scontato che ne esista uno ed uno solo
+		//       e che contenga un componente di tipo 'Renderer'
 		GameObject go = GameObject.Find ("Target");
+
+		// Traccio alla console il nome dell'oggetto appena trovato
+		Debug.Log ("Target: " + go);
+
+		// Recupero il componente di tipo 'Renderer' all'interno del gameobject appena trovato
+		// ed assegno al suo materiale quello dichiarato come 'targetMaterial'
 		go.GetComponent<Renderer> ().material = targetMaterial;
 
+		// Recupero una lista degli elementi in scena che siano stati etichettati come 'EnvironmentElement'
+		// NOTA: per semplificare do per scontato che ogni elemento della lista
+		//       contenga un componente di tipo 'Renderer'
 		GameObject[] goList = GameObject.FindGameObjectsWithTag ("EnvironmentElement");
+
+		// Ciclo sugli elementi appena trovati
 		foreach (GameObject element in goList) {
-			Debug.Log (element);
+			
+			// Traccio alla console il nome dell'elemento che sto considerando
+			Debug.Log ("Element: " + element);
+
+			// Recupero il componente di tipo 'Renderer' all'interno del gameobject 'element'
+			// ed assegno al suo materiale quello dichiarato come 'environmentMaterial'
 			element.GetComponent<Renderer> ().material = environmentMaterial;
 		}
-	}
-	
+	}	
 }
