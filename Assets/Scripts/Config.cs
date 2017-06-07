c9b63d19f015646f574e3397e8f4f59c0c0789fd
Marco Secchi <loziosecchi@gmail.com>
2017-06-07 19:00:04 +0200

    Add minimap. Add Random color generator. Add Config component.

---
+++ Assets/Scripts/Config.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Config : MonoBehaviour {
+
+	// Materiale degli oggetti in scena
+	// NOTA: 'SerializeField' ci assicura che l'attributo sia visibile
+	//       all'interno della scena. 'Private' rende inaccessibile l'attributo
+	//       all'esterno
+	[SerializeField]
+	private Material _environmentMaterial;
+
+	// Materiale dell'oggetto da raggiungere
+	[SerializeField]
+	private Material _targetMaterial;
+
+	// Materiale del personaggio
+	[SerializeField]
+	private Material _characterMaterial;
+
+	// Elenco dei colori disponibili per gli elementi dell'ambiente
+	[SerializeField]
+	private Color[] _environmentColors;
+
+	// Getter per il materiale dell'Environment
+	public Material EnvironmentMaterial {
+		get { 
+			return _environmentMaterial;
+		}
+	}
+
+	// Getter per il materiale del Target
+	public Material TargetMaterial {
+		get { 
+			return _targetMaterial;
+		}
+	}
+
+	// Getter per il materiale del Character (per la minimap)
+	public Material CharacterMaterial {
+		get { 
+			return _characterMaterial;
+		}
+	}
+
+	// Ritorna il colore all'indice 'index', assicurandosi che
+	// non sia un valore al difuori di quelli possibili
+	public Color GetEnvironmentColor(int index) {
+		if (index >= _environmentColors.Length)
+			index = 0;
+		return _environmentColors [index];
+	}
+
+	// Ritorna un colore a caso dalla lista di quelli disponibili
+	public Color GetRandomEnvironmentColor() {
+		var index = Random.Range (0, _environmentColors.Length);
+		return _environmentColors[index];
+	}
+}
--- Assets/Scripts/MyGameController.cs
+++ Assets/Scripts/MyGameController.cs
@@ -5,18 +5,17 @@ using UnityEngine;
 // Gestisce le logiche del gioco
 public class MyGameController : MonoBehaviour {
 
+	// Riferimento al componente 'Confic' che contiene
+	// gli elementi grafici per il gioco
+	public Config config;
+
 	// Indica se il gioco si trova in development mode,
 	// e quindi non andrà a sostituire i materiali
 	public bool devMode;
 
-	// Materiale degli oggetti in scena
-	public Material environmentMaterial;
-
-	// Materiale dell'oggetto da raggiungere
-	public Material targetMaterial;
-
 	// Callback di Unity per inizializzare la scena
 	void Start () {
+
 		// Se ci si trova in devMode, si esce dalla funzione
 		// senza effettuare nessuna operazione
 		if (devMode)
@@ -31,23 +30,43 @@ public class MyGameController : MonoBehaviour {
 		Debug.Log ("Target: " + go);
 
 		// Recupero il componente di tipo 'Renderer' all'interno del gameobject appena trovato
-		// ed assegno al suo materiale quello dichiarato come 'targetMaterial'
-		go.GetComponent<Renderer> ().material = targetMaterial;
+		// ed assegno al suo materiale quello dichiarato come 'TargetMaterial',
+		// recuperato dal componente Config
+		go.GetComponent<Renderer> ().material = config.TargetMaterial;
+
+		// Cerco in scena un oggetto etichettato 'MinimapCharacter'
+		// NOTA: per semplificare do per scontato che ne esista uno ed uno solo
+		//       e che contenga un componente di tipo 'Renderer'
+		GameObject character = GameObject.FindGameObjectWithTag ("MinimapCharacter");
+
+		// Recupero il componente di tipo 'Renderer' all'interno del gameobject appena trovato
+		// ed assegno al suo materiale quello dichiarato come 'CharacterMaterial',
+		// recuperato dal componente Config
+		character.GetComponent<Renderer> ().material = config.CharacterMaterial;
 
 		// Recupero una lista degli elementi in scena che siano stati etichettati come 'EnvironmentElement'
 		// NOTA: per semplificare do per scontato che ogni elemento della lista
 		//       contenga un componente di tipo 'Renderer'
 		GameObject[] goList = GameObject.FindGameObjectsWithTag ("EnvironmentElement");
 
+		// Recupero un colore casuale dal 'Config'
+		// NOTA: essendo dichiarato al di fuori del ciclo, ci assicura che
+		//       tutti gli elementi recuperati avranno lo stesso colore
+		Color c = config.GetRandomEnvironmentColor ();
+
 		// Ciclo sugli elementi appena trovati
 		foreach (GameObject element in goList) {
 			
 			// Traccio alla console il nome dell'elemento che sto considerando
-			Debug.Log ("Element: " + element);
+			Debug.Log ("Element: " + element.name);
 
 			// Recupero il componente di tipo 'Renderer' all'interno del gameobject 'element'
-			// ed assegno al suo materiale quello dichiarato come 'environmentMaterial'
-			element.GetComponent<Renderer> ().material = environmentMaterial;
+			// ed assegno al suo materiale quello dichiarato come 'EnvironmentMaterial',
+			// recuperato dal componente Config
+			element.GetComponent<Renderer> ().material = config.EnvironmentMaterial;
+
+			// Assegno all'elemento il colore generato a caso precedentemente
+			element.GetComponent<Renderer> ().material.color = c;
 		}
 	}	
 }
